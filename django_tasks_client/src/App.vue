95068d50b6741df18385901617f53ade16bfd6d4
KyleCapelli <66346910+KyleCapelli@users.noreply.github.com>
2022-06-23 11:23:48 +0800

    Add files for vue

---
+++ django_tasks_client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Tasks />
+  </div>
+</template>
+
+<script>
+import Tasks from './components/Tasks.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Tasks
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap');
+
+#app {
+  font-family: 'Josefin Sans', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
